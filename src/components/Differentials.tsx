bad0d1359265bc8598a84a00b8548378b74e2e59
spoliagency <mayara.spoli@gmail.com>
2025-08-04 13:30:43 -0300

    Atualizar texto para WhatsApp

--- src/components/Differentials.tsx
+++ src/components/Differentials.tsx
@@ -11,7 +11,7 @@ const Differentials = () => {
     },
     {
       icon: Clock,
-      title: "Disponível 24 horas no WhatsApp",
+      title: "Disponível 24h no seu WhatsApp",
       description: "Orion nunca dorme. Acesse via WhatsApp, web ou app, a qualquer hora do dia ou da noite.",
       color: "#21a1ff"
     },
